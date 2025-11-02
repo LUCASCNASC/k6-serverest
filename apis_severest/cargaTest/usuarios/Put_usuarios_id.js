bd6197af8c1ca55399cf6f3ab421e86f0b9f35be
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-11-02 10:42:20 -0300

    Desenvolvendo arquivos de teste.

---
+++ apis_severest/cargaTest/usuarios/Put_usuarios_id.js
@@ -0,0 +1,6 @@
+import http from 'k6/http'; 
+import { sleep, check } from 'k6';
+import { Counter, Gauge, Rate, Trend } from 'k6/metrics';
+
+const PATH_URL = '/Usuários/put_usuarios___id_';
+const response1 = http.put(`${__ENV.BASE_URL}/${PATH_URL}/1`);
